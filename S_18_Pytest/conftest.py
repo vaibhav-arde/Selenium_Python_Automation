b4ecbff915b2ab8b7c23e4b69aed39d0f85307a5
VaibhaV Arde <vaibhav.arde@gmail.com>
2024-09-25 15:37:17 +0530

    Pytest fixture

---
+++ S_18_Pytest/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+@pytest.fixture(scope="class")
+def setup():
+    print("I will be executing first")
+    yield
+    print(" I will execute last")
+
+
+@pytest.fixture()
+def dataLoad():
+    print("user profile data is being created")
+    return ["VaibhaV","Arde","vaibhavardeyacademy.com"]
+
+
+@pytest.fixture(params=[("chrome","VaibhaV","Arde"), ("Firefox","VaibhaV"), ("IE","VA")])
+def crossBrowser(request):
+    return request.param
