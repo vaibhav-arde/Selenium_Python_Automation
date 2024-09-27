1c2732f99dcd1325ca360fcd2126444541a67a30
VaibhaV Arde <vaibhav.arde@gmail.com>
2024-09-27 09:33:36 +0530

    Code to upload file from web

--- S_24_Miscellaneous/P01_Upload_Download.py
+++ S_24_Miscellaneous/P01_Upload_Download.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 import openpyxl
+import time
  
  
 def update_excel_data(filePath, searchTerm, colName, new_value):
@@ -36,13 +37,15 @@ driver.find_element(By.ID, "downloadButton").click()
 update_excel_data(file_path, fruit_name, "price", newValue)
  
 #upload
-# file_input = driver.find_element(By.CSS_SELECTOR,"input[type='file']")
-# file_input.send_keys(file_path)
+file_input = driver.find_element(By.CSS_SELECTOR,"input[type='file']")
+file_input.send_keys(file_path)
  
-# wait = WebDriverWait(driver,5)
-# toast_locator = (By.CSS_SELECTOR,".Toastify__toast-body div:nth-child(2)")
-# wait.until(expected_conditions.visibility_of_element_located(toast_locator))
-# print(driver.find_element(*toast_locator).text)
-# priceColumn = driver.find_element(By.XPATH,"//div[text()='Price']").get_attribute("data-column-id")
-# actual_price = driver.find_element(By.XPATH,"//div[text()='"+fruit_name+"']/parent::div/parent::div/div[@id='cell-"+priceColumn+"-undefined']").text
-# assert actual_price == newValue
+wait = WebDriverWait(driver,5)
+toast_locator = (By.CSS_SELECTOR,".Toastify__toast-body div:nth-child(2)")
+wait.until(expected_conditions.visibility_of_element_located(toast_locator))
+print(driver.find_element(*toast_locator).text)
+priceColumn = driver.find_element(By.XPATH,"//div[text()='Price']").get_attribute("data-column-id")
+actual_price = driver.find_element(By.XPATH,"//div[text()='"+fruit_name+"']/parent::div/parent::div/div[@id='cell-"+priceColumn+"-undefined']").text
+assert actual_price == newValue
+
+time.sleep(5)
